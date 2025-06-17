6effd6a416d6dccff17a7b91b2dd1aa225380a2b
Zsomething <nothanks@ontheemail.com>
2025-06-16 20:40:28 -0600

    fix stddev computation, add parser that prints out stddevs

---
+++ parse-results.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env pypy3
+
+import os, re, sys
+
+d = {}
+
+for fname in os.listdir("."):
+    # print("%r" % fname)
+    
+    for l in open(fname, "r").readlines():
+        l = l.strip()
+    
+        mo = re.search("^( *[^ ]*).*?([0-9]+)$", l)
+
+        if mo:
+            # print("mo.group(1): %r, mo.group(2): %r" % (mo.group(1), mo.group(2),))
+
+            fnname = mo.group(1)
+            stddev = int(mo.group(2))
+
+            # print("fn: %s, stddev: %s" % (fnname, stddev,))
+
+            d.setdefault(fnname, []).append(stddev)
+#         else:
+#             print("no match %r" % l)
+
+for (k, v) in d.items():
+    lis = list(v)
+    lis.sort()
+
+    # print("--- k: %r, v: %r" % (k, v,))
+    s = format(f"{k:21s}:")
+    for v in lis:
+        s += format(f" {v: 7d}")
+
+    print(s)
+       
+
--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,37 @@
 
 use std::time::Instant;
 use rustc_hash::FxHashMap;
-
 extern crate libc;
 
-const NUM_SAMPLES: u128 = 1000;
+const NUM_SAMPLES: u128 = 100_000_000;
 
-// const SLEEP_NANOS: u128 = 40_000;
-const SLEEP_NANOS: u128 = 0;
+pub fn dummy_func() -> i64 {
+    // When I make this code a little faster/simpler then gettime_cputime on Macos starts telling me
+    // that it took 0 nanoseconds. 🤔
+    let mut a = Arc::new(0);
+    for i in 0..30 {
+        for j in 0..29 {
+            *Arc::make_mut(&mut a) ^= black_box(i * j);
+        }
+    }
 
+    *a
+}
+
+use std::sync::Arc;
+use std::hint::black_box;
 fn instant() -> Vec<u128> {
-    eprintln!("instant");
-
     let mut durations = Vec::with_capacity(NUM_SAMPLES as usize);
-    let mut i = 0;
 
+    let mut i = 0;
     while i < NUM_SAMPLES {
         let inst = Instant::now();
 
-        thread::sleep(Duration::from_nanos(SLEEP_NANOS as u64));
+        black_box(dummy_func());
 
         let d = inst.elapsed();
-        let dif = d.as_nanos() - SLEEP_NANOS;
-        durations.push(dif);
+        assert!(d.as_nanos() > 0);
+        durations.push(d.as_nanos());
 
         i += 1;
     }
@@ -35,29 +44,28 @@ use std::mem::MaybeUninit;
 
 #[cfg(target_vendor = "apple")]
 pub mod plat_apple {
-    use crate::{NUM_SAMPLES, SLEEP_NANOS, thread, Duration, libc_gettime_clock, MaybeUninit};
+    use std::hint::black_box;
+    use crate::{NUM_SAMPLES, dummy_func, libc_gettime_clock, MaybeUninit};
     use libc::clockid_t;
     unsafe extern "C" {
         fn clock_gettime_nsec_np(clk_id: clockid_t) -> u64;
     }
 
-    pub fn measure_clock_gettime_nsec_np() -> Vec<u128> {
-        eprintln!("clock_gettime_nsec_np");
-
+    pub fn gettime_nsec_np() -> Vec<u128> {
         let mut durations = Vec::with_capacity(NUM_SAMPLES as usize);
         let mut i = 0;
     
         while i < NUM_SAMPLES {
             let prev = unsafe { clock_gettime_nsec_np(libc::CLOCK_UPTIME_RAW) };
 
-            thread::sleep(Duration::from_nanos(SLEEP_NANOS as u64));
+            black_box(dummy_func());
 
             let now = unsafe { clock_gettime_nsec_np(libc::CLOCK_UPTIME_RAW) };
 
-            let dur = now - prev;
-            let dif = dur - SLEEP_NANOS as u64;
+            let dur: u128 = u128::from(now - prev);
+            assert!(dur > 0);
             
-            durations.push(dif as u128);
+            durations.push(dur);
 
             i += 1;
         }
@@ -65,22 +73,19 @@ pub mod plat_apple {
         durations
     }
 
-    pub fn libc_gettime_uptime_raw() -> Vec<u128> {
-        eprintln!("libc_gettime_uptime_raw");
+    pub fn gettime_uptime_raw() -> Vec<u128> {
         libc_gettime_clock(libc::CLOCK_UPTIME_RAW)
     }
     
     use mach_sys::mach_time::{mach_absolute_time, mach_timebase_info};
     use mach_sys::kern_return::KERN_SUCCESS;
-    pub fn mat() -> Vec<u128> {
-        eprintln!("mach_absolute_time");
-
+    pub fn m_mach_absolute_time() -> Vec<u128> {
         let mut mtt1: MaybeUninit<mach_timebase_info> = MaybeUninit::uninit();
         let retval = unsafe { mach_timebase_info(mtt1.as_mut_ptr()) };
         assert_eq!(retval, KERN_SUCCESS);
         let mtt2 = unsafe { mtt1.assume_init() };
 
-        eprintln!("mach_timebase_info: {mtt2:?}");
+        //eprintln!("mach_timebase_info: {mtt2:?}");
 
         let mut durations = Vec::with_capacity(NUM_SAMPLES as usize);
         let mut i = 0;
@@ -88,13 +93,14 @@ pub mod plat_apple {
         while i < NUM_SAMPLES {
             let t1 = unsafe { mach_absolute_time() };
 
-            thread::sleep(Duration::from_nanos(SLEEP_NANOS as u64));
+            black_box(dummy_func());
 
             let t2 = unsafe { mach_absolute_time() };
+            assert!(t2 > t1);
 
-            let nanos = ((t2 - t1) * mtt2.numer as u64) / mtt2.denom as u64;
-            let dif = nanos - SLEEP_NANOS as u64;
-            durations.push(dif as u128);
+            let nanos: u128 = u128::from(((t2 - t1) * mtt2.numer as u64) / mtt2.denom as u64);
+            assert!(nanos > 0);
+            durations.push(nanos);
 
             i += 1;
         }
@@ -103,16 +109,15 @@ pub mod plat_apple {
     }
 }
     
-#[cfg(target_os = "macos")]
+#[cfg(target_vendor = "apple")]
 type ClockType = u32;
 
 #[cfg(target_os = "linux")]
 type ClockType = i32;
 
-use std::thread;
-use std::time::Duration;
-
 fn libc_gettime_clock(clock: ClockType) -> Vec<u128> {
+    extern crate libc;
+
     let mut durations = Vec::with_capacity(NUM_SAMPLES as usize);
     let mut i = 0;
     
@@ -122,24 +127,24 @@ fn libc_gettime_clock(clock: ClockType) -> Vec<u128> {
 
         let retval1 = unsafe { libc::clock_gettime(clock as ClockType, tp1.as_mut_ptr()) };
 
-        thread::sleep(Duration::from_nanos(SLEEP_NANOS as u64));
+        black_box(dummy_func());
 
         let retval2 = unsafe { libc::clock_gettime(clock as ClockType, tp2.as_mut_ptr()) };
 
         assert_eq!(retval1, 0);
         let instsec = unsafe { (*tp1.as_ptr()).tv_sec };
-
-        thread::sleep(Duration::from_nanos(SLEEP_NANOS as u64));
-
         let instnsec = unsafe { (*tp1.as_ptr()).tv_nsec };
         assert_eq!(retval2, 0);
         let newinstsec = unsafe { (*tp2.as_ptr()).tv_sec };
         let newinstnsec = unsafe { (*tp2.as_ptr()).tv_nsec };
 
-        let durnanos = (newinstsec - instsec) * 1_000_000_000 + newinstnsec - instnsec;
-        let dif = durnanos - SLEEP_NANOS as i64;
+        assert!(newinstsec * 1_000_000_000 + newinstnsec > instsec * 1_000_000_000 + instnsec, "newinstsec: {newinstsec}, newinstnsec: {newinstnsec}, instsec: {instsec}, instnsec: {instnsec}");
+        let durnanosi64 = (newinstsec - instsec) * 1_000_000_000 + newinstnsec - instnsec;
+        assert!(durnanosi64 > 0);
+        let durnanos: u128 = durnanosi64.try_into().unwrap();
+        assert!(durnanos > 0);
 
-        durations.push(dif as u128);
+        durations.push(durnanos);
 
         i += 1;
     }
@@ -147,45 +152,40 @@ fn libc_gettime_clock(clock: ClockType) -> Vec<u128> {
     durations
 }
     
-fn libc_gettime_cputime() -> Vec<u128> {
-    eprintln!("libc_gettime_cputime");
+fn gettime_cputime() -> Vec<u128> {
     libc_gettime_clock(libc::CLOCK_PROCESS_CPUTIME_ID)
 }
 
-fn libc_gettime_monotonic() -> Vec<u128> {
-    eprintln!("libc_gettime_monotonic");
+fn gettime_monotonic() -> Vec<u128> {
     libc_gettime_clock(libc::CLOCK_MONOTONIC)
 }
     
-fn libc_gettime_monotonic_raw() -> Vec<u128> {
-    eprintln!("libc_gettime_monotonic_raw");
+fn gettime_monotonic_raw() -> Vec<u128> {
     libc_gettime_clock(libc::CLOCK_MONOTONIC_RAW)
 }
     
 #[cfg(target_arch = "x86_64")]
 pub mod plat_x86_64 {
-    use crate::{NUM_SAMPLES, SLEEP_NANOS, thread, Duration};
+    use crate::{NUM_SAMPLES, thread, Duration};
     use core::arch::x86_64;
 
     use cpuid;
 
-    pub fn measure_rdtscp() -> Vec<u128> {
-        eprintln!("rdtscp");
-
+    pub fn rdtscp() -> Vec<u128> {
         let mut durations = Vec::with_capacity(NUM_SAMPLES as usize);
         let mut i = 0;
     
         let ofreq = cpuid::clock_frequency();
         assert!(ofreq.is_some());
         let mut prev_freq_mhz = ofreq.unwrap();
-        eprintln!("freq {} MHz", prev_freq_mhz);
+        //eprintln!("freq {} MHz", prev_freq_mhz);
 
         while i < NUM_SAMPLES {
             let mut aux1 = 0;
             let mut aux2 = 0;
             let now1 = unsafe { x86_64::__rdtscp(&mut aux1) };
 
-            thread::sleep(Duration::from_nanos(SLEEP_NANOS as u64));
+            black_box(dummy_func());
 
             let now2 = unsafe { x86_64::__rdtscp(&mut aux2) };
 
@@ -199,10 +199,10 @@ pub mod plat_x86_64 {
             assert_eq!(prev_freq_mhz, freq_mhz); // frequency changed
             prev_freq_mhz = freq_mhz;
 
-            let durnanos = durcycles * 1000 / freq_mhz as u64;
-            let dif = durnanos - SLEEP_NANOS as u64;
+            let durnanos = durcycles * 1000 / freq_mhz;
+            assert!(durnanos > 0);
             
-            durations.push(dif as u128);
+            durations.push(durnanos);
 
             i += 1;
         }
@@ -211,8 +211,7 @@ pub mod plat_x86_64 {
     }
 }
 
-use thousands::Separable;
-fn stats<F>(func: F)
+fn stats<F>(func: F, fnname: &str)
 where
     F: Fn() -> Vec<u128>,
 {
@@ -228,34 +227,63 @@ where
 
     pairs.sort_by(|a, b| a.0.cmp(b.0));
 
-    println!("{:>10} {:>10} {:>10} {:>10}", "nanos", "#", "# < nanos", "# > nanos");
-    println!("{:>10} {:>10} {:>10} {:>10}", "-----", "-", "---------", "---------");
-    let mut sum = 0;
-    for (key, value) in pairs {
-        println!("{:>10} {:>10} {:>10} {:>10}", key.separate_with_commas(), value.separate_with_commas(), (value + sum).separate_with_commas(), (NUM_SAMPLES - value - sum).separate_with_commas());
-        sum += value;
+    let mut perc50: u128 = 0;
+    let mut perc95: u128 = 0;
+    let min: u128 = *pairs[0].0;
+
+    //println!("{:>10} {:>10} {:>10} {:>10}", "nanos", "#", "# < nanos", "# > nanos");
+    //println!("{:>10} {:>10} {:>10} {:>10}", "-----", "-", "---------", "---------");
+    let mut sumnanos = 0;
+    let mut sumnums = 0;
+    for (nanos, num) in &pairs {
+        //println!("{:>10} {:>10} {:>10} {:>10}", nanos.separate_with_commas(), num.separate_with_commas(), (num + sumnums).separate_with_commas(), (NUM_SAMPLES - num - sumnums).separate_with_commas());
+        if (sumnums + *num >= NUM_SAMPLES * 95 / 100) && (sumnums < NUM_SAMPLES * 95 / 100) {
+            //println!("95 percentile");
+            perc95 = **nanos;
+        }
+        if (sumnums + *num >= NUM_SAMPLES * 50 / 100) && (sumnums < NUM_SAMPLES * 50 / 100) {
+            //println!("50 percentile");
+            perc50 = **nanos;
+        }
+        sumnanos += *nanos * *num;
+        sumnums += *num;
     }
+    assert!(sumnanos < i128::MAX as u128);
+    let mean: i128 = (sumnanos / NUM_SAMPLES).try_into().unwrap();
+
+    if perc50 == 0 {
+        perc50 = *pairs[pairs.len()-1].0;
+    }
+    if perc95 == 0 {
+        perc95 = *pairs[pairs.len()-1].0;
+    }
+
+    let mut sumsquares = 0;
+    for (nanos, num) in pairs {
+        //println!("{:>10} {:>10} {:>10} {:>10}", nanos.separate_with_commas(), num.separate_with_commas(), (num + sum).separate_with_commas(), (NUM_SAMPLES - num - sum).separate_with_commas());
+        let diff: i128 = *nanos as i128 - mean;
+        let sqdiff: u128 = diff.pow(2).try_into().unwrap();
+        sumsquares += sqdiff * *num;
+    }
+    let stddev = (sumsquares / (NUM_SAMPLES - 1)).isqrt();
+
+    println!("{fnname:>21} {min:>6} {perc50:>6} {perc95:>6} {mean:>6} {stddev:>7}");
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("{:>21} {:>6} {:>6} {:>6} {:>6} {:>7}", "fnname", "min", "50th%", "95th%", "mean", "stddev");
+    println!("{:>21} {:>6} {:>6} {:>6} {:>6} {:>7}", "------", "---", "-----", "-----", "----", "------");
 
-    stats(instant);
-    eprintln!();
-    stats(libc_gettime_cputime);
-    eprintln!();
-    stats(libc_gettime_monotonic);
-    eprintln!();
-    stats(libc_gettime_monotonic_raw);
-    eprintln!();
+    stats(instant, "instant");
+    stats(gettime_cputime, "gettime_cputime");
+    stats(gettime_monotonic, "gettime_monotonic");
+    stats(gettime_monotonic_raw, "gettime_monotonic_raw");
 #[cfg(target_vendor = "apple")]
     {
-        stats(plat_apple::libc_gettime_uptime_raw);
-        eprintln!();
-        stats(plat_apple::mat);
-        eprintln!();
-        stats(plat_apple::measure_clock_gettime_nsec_np);
+        stats(plat_apple::gettime_uptime_raw, "gettime_uptime_raw");
+        stats(plat_apple::m_mach_absolute_time, "mach_absolute_time");
+        stats(plat_apple::gettime_nsec_np, "gettime_nsec_np");
     }
 #[cfg(target_arch = "x86_64")]
-    stats(plat_x86_64::measure_rdtscp);
+    stats(plat_x86_64::rdtscp, "rdtscp");
 }
