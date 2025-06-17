b78d6b348c75932b000bedb64cbc7fe4242bd058
Zsomething <nothanks@ontheemail.com>
2025-06-17 17:29:47 -0600

    see intermediate results printed out as it is running

--- run-measurement.sh
+++ run-measurement.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-OS=macos
-PROC=m4max
-LOGF=./results/$OS-$PROC-`date -u "+%Y-%m-%d_%H-%M-%S"`.txt; ./target/release/measure-clocks > $LOGF 2>&1 ; cat $LOGF
+OS=unknownos
+PROC=unknownproc
+LOGF=./results/$OS-$PROC-`date -u "+%Y-%m-%d_%H-%M-%S"`.txt; ./target/release/measure-clocks | tee $LOGF 2>&1
