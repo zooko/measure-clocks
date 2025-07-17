1d81d6ff2373473bd6f8f892b316679eef81d8ef
Zsomething <nothanks@ontheemail.com>
2025-07-17 15:38:44 -0600

    measure CLOCK_MONOTONIC_COARSE just cause

--- src/main.rs
+++ src/main.rs
@@ -181,6 +181,11 @@ fn monotonic_raw() -> Vec<u128> {
     libc_gettime_clock(libc::CLOCK_MONOTONIC_RAW)
 }
     
+#[cfg(target_os = "linux")]
+fn monotonic_coarse() -> Vec<u128> {
+    libc_gettime_clock(libc::CLOCK_MONOTONIC_COARSE)
+}
+    
 #[cfg(target_arch = "x86_64")]
 pub mod plat_x86_64 {
     use crate::{dummy_func, NUM_SAMPLES};
@@ -311,4 +316,6 @@ fn main() {
     }
 #[cfg(target_arch = "x86_64")]
     stats(plat_x86_64::rdtscp, "rdtscp");
+#[cfg(target_os = "linux")]
+    stats(monotonic_coarse, "monotonic_coarse");
 }
